7a4fe13c9febdfaa88ba562d9732892753c1124a
shrawan dhawale <shrawan.dhawale@gmail.com>
2026-02-12 22:03:48 +0530

    Initial commit

---
+++ src/main/java/com/_spring_boot/spring_meven/SpringMevenApplication.java
@@ -0,0 +1,13 @@
+package com._spring_boot.spring_meven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMevenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_spring_boot/spring_meven/SpringMevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com._spring_boot.spring_meven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
