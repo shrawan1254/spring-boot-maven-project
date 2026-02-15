cbf660bdeb77e55f8fa4a48d5f22c043d0a5331e
shrawan dhawale <shrawan.dhawale@gmail.com>
2026-02-15 23:03:13 +0530

    Add simple front end and HelloController

---
+++ src/main/java/com/_spring_boot/spring_meven/HelloController.java
@@ -0,0 +1,13 @@
+package com._spring_boot.spring_meven;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from the back end!";
+    }
+}
